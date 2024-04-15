98dd8160f0b0751849219f1ce830f5c302ff5c54
ILYA GRIGOREV <lizarthej@gmail.com>
2024-04-15 15:26:02 +0300

    second-commit

--- html__css/моя_робота/piramida/js/index.js
+++ html__css/моя_робота/piramida/js/index.js
@@ -1,9 +1,9 @@
 const btnNext = document.querySelector("#nextButton");
 const sectionTwo = document.querySelector(".top2");
-
+const positionY = sectionTwo.getBoundingClientRect().y;
 
 btnNext.addEventListener("click", () => {
-    const positionY = sectionTwo.getBoundingClientRect().y;
+
     window.scrollTo(0, positionY);
 })
 
--- html__css/моя_робота/piramida/js/index2.js
+++ html__css/моя_робота/piramida/js/index2.js
@@ -1,8 +1,8 @@
 const btnNext2 = document.querySelector("#nextButton2");
 const sectionThree = document.querySelector(".top3");
+const position2 = sectionThree.getBoundingClientRect().y;
 
 
 btnNext2.addEventListener("click", () => {
-    const position2 = sectionThree.getBoundingClientRect();
-    window.scrollTo(0, position2.y);
+    window.scrollTo(0, position2);
   })
